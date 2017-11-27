f1d26703a2ba5d889bf10c5648f35f5ae89b79a3
Moisés S. E. Aguilar <moisesmagno@gmail.com>
2017-11-27 09:33:17 -0200

    Criado os sitemas de autenticãção e registro do Laravel.

--- routes/web.php
+++ routes/web.php
@@ -6,3 +6,7 @@ $this->get('panel', 'Panel\PanelController@index');
 //SITE
 $this->get('/', 'Site\SiteController@index')->name('home');
 $this->get('promocoes', 'Site\SiteController@promotions')->name('promotions');
+
+
+Auth::routes();
+
